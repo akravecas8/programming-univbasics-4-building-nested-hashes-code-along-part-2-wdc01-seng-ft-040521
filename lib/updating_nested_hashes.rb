bf6e2144bfd00d9acbbfdbf0463cd6beb44653dc
casual-karma-e108 <akravecas@yahoo.com>
2021-02-19 17:47:55 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,7 +61,7 @@ def add_characters
    apothecary = {name: "Apothecary"}
    
    epic_tragedy[:additional_characters] << prince
-   epic_tragedy{:additional_characters] << apothecary
+   epic_tragedy[:additional_characters] << apothecary
    
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
